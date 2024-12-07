96fa5fd4df05a534999afa8a90041ffc6f54d6ab
Luke Steinbicker <lukesteinbicker@gmail.com>
2024-12-07 18:50:38 -0500

    Reset repository to match local files

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,12 @@ export default async function Index() {
           <div className="h-[20rem] flex flex-col justify-start items-center px-4">
             <div className="text-2xl mx-auto font-normal text-muted-foreground mb-20">
               <h2>
-                Have you ever been certain that something will happen but don&apos;t have the ability or courage to bet on it? This application records your predictions on the Ethereum network along with their timestamps so you can prove to your friends you came up with that 10-leg parlay BEFORE it hit.
-                If you have a bad gambling habit or just want to show others your knack for speculating on stock prices, game outcomes, or any other event, this is perfect for you. <Link href="/sign-up"><u>Sign up and create predictions for free in seconds.</u></Link>
+                The creator of this project didn&apos;t want to pay his email verification API.
+                New accounts cannot be created, but feel free to check out the source code.
               </h2>
             </div>
             <div className="text-sm mx-auto font-normal text-muted-foreground">
-              <p>If you don't want to create an account, <Link href="/manual-transaction"><u>you can still make transations with the contract.</u></Link></p>
+              <p><Link href="/manual-transaction"><u>You can still make transations with the contract, too.</u></Link></p>
             </div>
           </div>
         </div>
